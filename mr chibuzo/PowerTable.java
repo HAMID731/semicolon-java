3d5fd68620b00c903cd994efbdcb450833dcd5b1
HAMID <abdulhamidabari652@gmail.com>
2024-10-21 05:43:37 +0100

    powertable

---
+++ mr chibuzo/PowerTable.java
@@ -0,0 +1,17 @@
+public class PowerTable {
+
+public static void main(String[] args) {
+double number1 = Math.pow(1,2);
+double number2 = Math.pow(2,3);
+double number3 = Math.pow(3,4);
+double number4 = Math.pow(4,5);
+double number5 = Math.pow(5,6);
+
+System.out.println("a            b         pow(a,b)");
+System.out.println("1            2        "+number1);
+System.out.println("2            3        "+number2);
+System.out.println("3            4        "+number3);
+System.out.println("4            5        "+number4);
+System.out.println("5            6        "+number5);
+
+}}
