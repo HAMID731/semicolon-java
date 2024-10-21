a6d45535603351665e03fda76b3b31add5fc1b6a
HAMID <abdulhamidabari652@gmail.com>
2024-10-21 06:48:41 +0100

    counter

---
+++ mr chibuzo/counter.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class counter{
+
+public static void main(String[]args){
+
+Scanner scan = new Scanner(System.in);
+int negative =0;
+int positive =0;
+int zero =0;
+int counter =1;
+
+
+while ( counter<=5 ){
+System.out.println("enter number: ");
+int number = scan.nextInt();
+
+if(number<0){
+negative++;
+}
+else if(number==0){
+zero++;
+}
+else{positive++;}
+
+System.out.println("there are"+negative+"numbers");
+System.out.println("there are"+positive+"numbers");
+System.out.println("there are"+zero+"numbers");
+}
+}}
