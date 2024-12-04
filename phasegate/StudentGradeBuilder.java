3264b9e7cb02fd788a459f6d7dcc78401f2f6566
HAMID <abdulhamidabari652@gmail.com>
2024-12-04 20:57:53 +0100

    now

--- phasegate/StudentGradeBuilder.java
+++ phasegate/StudentGradeBuilder.java
@@ -1,52 +1,35 @@
 import java.util.Scanner;
-import java.util.ArrayList;
-public class StudentGradeBuilder{
 
-public static void main(String[]args){
-	Scanner scan = new Scanner(System.in);
-	System.out.println("How many student do you have: ");
-	int number = scan.nextInt();
-	System.out.println("How many subject do they offer: ");
-	int  subject= scan.nextInt();
+public class StudentGradeBuilder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("How many student do you have: \n");
+        int students = scanner.nextInt();
+        System.out.print("How many student do they offer: \n");
+        int subjects = scanner.nextInt();
 
-	int[] numberOfSubject = new int[subject];
-	int[] numberOfStudent = new int[number];
-	System.out.print("saving >>>>>>>>>>>>>>>> ");
-	System.out.print("\nsaved successfully ");
-	System.out.println();
-	ArrayList<Integer> list = new ArrayList<Integer>();
+        int[][] scores = new int[students][subjects];
 
-	for (int student = 1; student < numberOfStudent.length+1; student++){
+        for (int i = 0; i < students; i++) {
+            System.out.println("Entering score for student " + (i + 1) + ":");
+            for (int j = 0; j < subjects; j++) {    
+		int score;
+                do {
+                    System.out.println("Enter score for subject " + (j + 1) + ":");
+                    score = scanner.nextInt();
+                } while (score < 0 || score > 100);
+                scores[i][j] = score;
+            }
+        }
 
-		for (int subjectOffer = 1; subjectOffer < numberOfSubject.length+1; subjectOffer++){
-
-			int score ;
-			System.out.println("\nEntering score for student "+student+"\nEntering score for subject "+ subjectOffer);
-			score = scan.nextInt();
-			list.add(score);
-			System.out.print("saving >>>>>>>>>>>>>>>> ");
-			System.out.print("\nsaved successfully \n");
-		for (int i = 0; i < list.size(); i++) {
-			System.out.print("----------------------------------------------------------------------------------------------\n");
-			System.out.print("----------------------------------------------------------------------------------------------\n");
-			getSubject(subjectOffer);
-			System.out.print("----------------------------------------------------------------------------------------------\n");
-
-		}
-
-		}
-		
-		}
-
-	
+        System.out.println("\nClass Summary:");
+        for (int i = 0; i < students; i++) {
+            System.out.print("Student " + (i + 1) + ": ");
+            int total = 0;
+            for (int j = 0; j < subjects; j++) {
+                total += scores[i][j];
+            }
+            System.out.println("Total = " + total + ", Average = " + (total / subjects));
+        }
+    }
 }
-	public static void getSubject(int subject){
-		int[] numberOfSubject = new int[subject];
-		for (int subjectOffer = 1; subjectOffer < numberOfSubject.length+1; subjectOffer++){
-			System.out.print("sub"+subjectOffer+1);
-		}
-}
-	
-		
-}
-			
